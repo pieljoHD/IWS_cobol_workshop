86147b1e6ff5a17c23f8910d2f53440b5224e60d
EZ <ermir959@gmail.com>
2025-12-01 16:34:32 +0100

    .exe removed + commented file added + compiler fixed

--- 1-Lesson/compile-and-run.sh
+++ 1-Lesson/compile-and-run.sh
@@ -1,1 +1,11 @@
-clear && cobc -x stundensatz.cbl && ./stundensatz
+# Bildschirm leeren, damit die Ausgabe übersichtlich ist
+clear && printf "\033c"
+
+# COBOL-Datei übersetzen (kompilieren): 
+# -x = ausführbares Programm erzeugen,
+# -free = Free-Format akzeptieren (Code nicht an feste spalten gebunden), 
+# -o = Name des erzeugten Programms ("stundensatz")
+cobc -x -free -o stundensatz_kommentiert stundensatz_kommentiert.cbl
+
+# Das frisch erstellte COBOL-Programm starten
+./stundensatz_kommentiert
